6f57324dc3057e821e43d818ea9c7f63198e62ee
alejandro <alejandrodanielaquino@yahoo.com.ar>
2020-12-29 17:57:14 -0300

    Add contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,42 @@
 import React from "react";
 import Seo from "./Seo";
+import splash from "../images/splash.svg";
 
 function Contact() {
   return (
-    <>
+    <section className="form-page">
       <Seo title="Contact" />
-      <h1>Contact</h1>
-    </>
+      <div className="form-wrapper">
+        <h3 className="contact-me">Contact me</h3>
+        <form className="contact-form">
+          <div className="form-section">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" placeholder="Lionel" />
+          </div>
+          <div className="form-section">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="lionelmessi@gmail.com"
+            />
+          </div>
+
+          <div className="form-section">
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              cols="90"
+              rows="15"
+            ></textarea>
+          </div>
+          <button type="button">Send me a Message!</button>
+        </form>
+      </div>
+      <img src={splash} alt="decoration" className="background" />
+    </section>
   );
 }
 
