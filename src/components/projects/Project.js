be2e886fac1b10c721b79b746a337814f2dfab20
alejandro <alejandrodanielaquino@yahoo.com.ar>
2021-01-06 15:36:35 -0300

    add styles to projects

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -3,9 +3,9 @@ import React from "react";
 function project({ className, image, altName, title, technology, href }) {
   return (
     <div className={`project ${className}`}>
-      <h2>
+      <h3>
         {title} - {technology}
-      </h2>
+      </h3>
       <a href={href} rel="noreferrer" target="_blank">
         <img src={image} alt={altName} className="project-image" />
       </a>
