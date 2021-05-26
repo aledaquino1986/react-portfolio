369ab30ea02f2349e8daf462ca6c1f93c0207919
alejandro <alejandrodanielaquino@yahoo.com.ar>
2021-05-26 00:53:23 -0300

    Remove Lazy component initialization

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./styles/app.scss";
+import Homepage from "./pages/homepage/Homepage";
+import Contact from "./pages/contact/Contact";
+import Projects from "./pages/projects/Projects";
+import Error from "./pages/error/Error";
+
 import Layout from "./components/layout/Layout";
-
-const Homepage = React.lazy(() => import("./pages/homepage/Homepage"));
-const Contact = React.lazy(() => import("./pages/contact/Contact"));
-const Projects = React.lazy(() => import("./pages/projects/Projects"));
-const Error = React.lazy(() => import("./pages/error/Error"));
-
 function App() {
   return (
     <BrowserRouter>
       <Layout>
-        <React.Suspense fallback={<h2>Loading...</h2>}>
-          <Switch>
-            <Route exact path="/">
-              <Homepage />
-            </Route>
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
 
-            <Route path="/projects">
-              <Projects />
-            </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
 
-            <Route path="/contact">
-              <Contact />
-            </Route>
-            <Route path="*">
-              <Error />
-            </Route>
-          </Switch>
-        </React.Suspense>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
       </Layout>
     </BrowserRouter>
   );
